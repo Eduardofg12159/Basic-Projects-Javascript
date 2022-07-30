21dd61d74e126dabaa48f7340f9cdb6dddb54bfe
Juan Eduardo Flores <eduardofg12159@gmail.com>
2022-07-30 19:34:58 +0100

    ADD: Reviews and counter project added

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,5 +1,6 @@
-// set inital value to zero
+// set initial count
 let count = 0;
+
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
@@ -14,14 +15,11 @@ btns.forEach(function (btn) {
     } else {
       count = 0;
     }
-
     if (count > 0) {
       value.style.color = "green";
-    }
-    if (count < 0) {
+    } else if (count < 0) {
       value.style.color = "red";
-    }
-    if (count === 0) {
+    } else {
       value.style.color = "#222";
     }
     value.textContent = count;
